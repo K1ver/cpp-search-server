a4d54716dc256f003eaf1efa1829120f63d88cb1
K1ver <danil.puchkov.1993@mail.ru>
2023-07-04 12:33:13 +0700

    Fix remove_duplicates

--- search-server/remove_duplicates.cpp
+++ search-server/remove_duplicates.cpp
@@ -5,12 +5,9 @@ void RemoveDuplicates(SearchServer& search_server) {
     std::set<int> id_remove;
     std::map<std::set<std::string>, int> unique_word_plus_id;
     for (const int document_id : search_server) {
-        std::map<std::string, double> all_words;
- 
-        all_words = search_server.GetWordFrequencies(document_id);
         std::set<std::string> unique_words;
  
-        for (auto [word, _] : all_words) {
+        for (auto [word, _] : search_server.GetWordFrequencies(document_id)) {
             unique_words.insert(word);
         }
  
@@ -20,8 +17,8 @@ void RemoveDuplicates(SearchServer& search_server) {
             unique_word_plus_id.insert(std::pair{unique_words, document_id});
         }
     }
-    for (auto id_: id_remove) {
-        std::cout<<"Found duplicate document id "<< id_ <<std::endl;
-        search_server.RemoveDocument(id_);
+    for (auto id: id_remove) {
+        std::cout<<"Found duplicate document id "<< id <<std::endl;
+        search_server.RemoveDocument(id);
     }
 }
